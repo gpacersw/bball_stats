8cf81908f4d1f5ccf24dd572adc0c101a8d9e0b3
Gregg Ganley <gganley@foo.com>
2013-03-01 10:29:26 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BballStats::Application.config.secret_token = 'a75c310f5cd08d5ba16a51c8306f610fa253fba6a90575d7715a92d96b3bc0e003a179ccf5c455ced639199cf4a717a85180a7cf04a366f2331474dd70568f73'
